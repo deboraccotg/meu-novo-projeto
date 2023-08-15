f57213e4cb35472aaa5bbc120ef95c28a6db1e44
Debora <Debora@DESKTOP-G68OI5K>
2023-08-15 00:02:34 -0300

    recuperacaoconstante

---
+++ primeiro-program-java/src/cursojava/constantes/StatusAluno.java
@@ -0,0 +1,10 @@
+package cursojava.constantes;
+/*Classe e constante estáticas*/
+
+public class StatusAluno {
+	public static String APROVADO="Aprovado";
+	public static String RECUPERACAO="RECUPERACAO=";
+	public static String REPROVADO="REPROVADO";
+
+
+}
